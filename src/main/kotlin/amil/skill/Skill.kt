e1a472a3fc61b356169791bce2e18acb177a745d
PleahMaCaka <kkjuh0905@gmail.com>
2022-10-02 04:07:31 +0900

    rapier

--- src/main/kotlin/amil/skill/Skill.kt
+++ src/main/kotlin/amil/skill/Skill.kt
@@ -1,5 +1,6 @@
 package amil.skill
 
+import amil.skill.weapon.Rapier
 import org.bukkit.plugin.java.JavaPlugin
 
 @Suppress("unused")
@@ -7,7 +8,11 @@ class Skill : JavaPlugin() {
 
     override fun onEnable() {
         logger.info("Skill Plugins Enabled")
+        // test
         server.pluginManager.registerEvents(TestSkill, this)
+
+        // weapons
+        server.pluginManager.registerEvents(Rapier, this)
     }
 
     override fun onDisable() {
