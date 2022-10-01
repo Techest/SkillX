82022327697339affc65dcc822a97825b9baa2e9
PleahMaCaka <kkjuh0905@gmail.com>
2022-10-02 01:41:16 +0900

    refactor: split skill test

--- src/main/kotlin/amil/skill/Skill.kt
+++ src/main/kotlin/amil/skill/Skill.kt
@@ -1,69 +1,17 @@
 package amil.skill
 
-import org.bukkit.Material
-import org.bukkit.Particle
-import org.bukkit.event.EventHandler
-import org.bukkit.event.Listener
-import org.bukkit.event.block.Action
-import org.bukkit.event.player.PlayerInteractEvent
 import org.bukkit.plugin.java.JavaPlugin
-import java.lang.Double.min
 
-class Skill : JavaPlugin(), Listener {
+@Suppress("unused")
+class Skill : JavaPlugin() {
+
     override fun onEnable() {
-        logger.info("스타또")
-        server.pluginManager.registerEvents(this, this)
+        logger.info("Skill Plugins Enabled")
+        server.pluginManager.registerEvents(TestSkill, this)
     }
 
     override fun onDisable() {
-    }
-
-    @EventHandler
-    fun onRightClick(event: PlayerInteractEvent) {
-        val player = event.player
-        val loc = player.location
-
-        if (event.item?.type != Material.STICK) return
-
-        when (event.action) {
-            Action.LEFT_CLICK_BLOCK -> {
-
-            }
-
-            // Replace block to log
-            Action.RIGHT_CLICK_BLOCK -> {
-                event.clickedBlock?.type = Material.BIRCH_LOG
-            }
-
-            // spawn particle
-            Action.LEFT_CLICK_AIR -> {
-                player.health = min(player.health + 10, 20.0) // <- 스파게티 방쉭
-                for (i in 1 .. 20)
-                player.spawnParticle(
-                    Particle.HEART,
-                    loc.x + (player.eyeLocation.direction.x * i),
-                    loc.y + (player.eyeLocation.direction.y * i),
-                    loc.z + (player.eyeLocation.direction.z * i),
-                    1
-                )
-            }
-
-            // dash
-            Action.RIGHT_CLICK_AIR -> {
-                player.velocity = player.velocity.add(player.location.direction.multiply(2))
-                player.spawnParticle(Particle.PORTAL, player.location, 1000, 1.0, 1.0, 1.0)
-            }
-
-            Action.PHYSICAL -> return
-        }
-
-        // 허공 좌클릭 스킬 ( 검기 날리기 )
-        if (
-            player.isSneaking &&
-            event.action == Action.LEFT_CLICK_AIR || event.action == Action.LEFT_CLICK_BLOCK
-        ) {
-            player.sendMessage("Shift + Left Click")
-        }
+        logger.info("Skill Plugins Disabled")
     }
 
 }
---
+++ src/main/kotlin/amil/skill/TestSkill.kt
@@ -0,0 +1,58 @@
+package amil.skill
+
+import org.bukkit.Material
+import org.bukkit.Particle
+import org.bukkit.event.EventHandler
+import org.bukkit.event.Listener
+import org.bukkit.event.block.Action
+import org.bukkit.event.player.PlayerInteractEvent
+import kotlin.math.min
+
+object TestSkill : Listener {
+
+    @EventHandler
+    fun onRightClick(event: PlayerInteractEvent) {
+        val player = event.player
+        val loc = player.location
+
+        if (event.item?.type != Material.STICK) return
+
+        when (event.action) {
+            Action.LEFT_CLICK_BLOCK -> return
+
+            // Replace block to log
+            Action.RIGHT_CLICK_BLOCK ->
+                event.clickedBlock?.type = Material.BIRCH_LOG
+
+            // spawn particle
+            Action.LEFT_CLICK_AIR -> {
+                player.health = min(player.health + 10, 20.0) // <- 스파게티 방쉭
+                for (i in 1..20)
+                    player.spawnParticle(
+                        Particle.HEART,
+                        loc.x + (player.eyeLocation.direction.x * i),
+                        loc.y + (player.eyeLocation.direction.y * i),
+                        loc.z + (player.eyeLocation.direction.z * i),
+                        1
+                    )
+            }
+
+            // dash
+            Action.RIGHT_CLICK_AIR -> {
+                player.velocity = player.velocity.add(player.location.direction.multiply(2))
+                player.spawnParticle(Particle.PORTAL, player.location, 1000, 1.0, 1.0, 1.0)
+            }
+
+            Action.PHYSICAL -> return
+        }
+
+        // 허공 좌클릭 스킬 ( 검기 날리기 )
+        if (
+            player.isSneaking &&
+            event.action == Action.LEFT_CLICK_AIR || event.action == Action.LEFT_CLICK_BLOCK
+        ) {
+            player.sendMessage("Shift + Left Click")
+        }
+    }
+
+}
