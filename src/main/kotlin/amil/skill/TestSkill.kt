eae91602d67864edf5c5362adbce6c4cd51d9f78
DwarfAmil <hjp7126@naver.com>
2022-10-02 03:45:55 +0900

    Shift + Left Click Skill

--- src/main/kotlin/amil/skill/TestSkill.kt
+++ src/main/kotlin/amil/skill/TestSkill.kt
@@ -2,6 +2,7 @@ package amil.skill
 
 import org.bukkit.Material
 import org.bukkit.Particle
+import org.bukkit.Sound
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.block.Action
@@ -17,6 +18,23 @@ object TestSkill : Listener {
 
         if (event.item?.type != Material.STICK) return
 
+        // 허공 좌클릭 스킬 ( 검기 날리기 )
+        if (player.isSneaking &&
+            event.action == Action.LEFT_CLICK_AIR || event.action == Action.LEFT_CLICK_BLOCK) {
+            for (i in 1..5)
+                player.spawnParticle(
+                    Particle.SONIC_BOOM,
+                    loc.x + (player.eyeLocation.direction.x * i),
+                    loc.y + (player.eyeLocation.direction.y * i),
+                    loc.z + (player.eyeLocation.direction.z * i),
+                    1
+                )
+
+            player.velocity = player.velocity.add(player.location.direction.multiply(-2))
+
+            return
+        }
+
         when (event.action) {
             Action.LEFT_CLICK_BLOCK -> return
 
@@ -29,12 +47,14 @@ object TestSkill : Listener {
                 player.health = min(player.health + 10, 20.0) // <- 스파게티 방쉭
                 for (i in 1..20)
                     player.spawnParticle(
-                        Particle.HEART,
+                        Particle.SONIC_BOOM,
                         loc.x + (player.eyeLocation.direction.x * i),
                         loc.y + (player.eyeLocation.direction.y * i),
                         loc.z + (player.eyeLocation.direction.z * i),
                         1
                     )
+
+                player.playSound(player.getLocation(), Sound.ENTITY_WARDEN_SONIC_BOOM, 1.0f, 10.0f)
             }
 
             // dash
@@ -44,14 +64,6 @@ object TestSkill : Listener {
             }
 
             Action.PHYSICAL -> return
-        }
-
-        // 허공 좌클릭 스킬 ( 검기 날리기 )
-        if (
-            player.isSneaking &&
-            event.action == Action.LEFT_CLICK_AIR || event.action == Action.LEFT_CLICK_BLOCK
-        ) {
-            player.sendMessage("Shift + Left Click")
         }
     }
 
