e8d2de2e095e1417137738ff80e8fea6ee5f0f4b
PleahMaCaka <pleahmacaka@gmail.com>
2022-10-30 21:22:10 +0900

    :star:

---
+++ src/main/kotlin/skillx/interfaces/IWeapon.kt
@@ -0,0 +1,76 @@
+import org.bukkit.Material
+import org.bukkit.event.EventHandler
+import org.bukkit.event.Listener
+import org.bukkit.event.block.Action
+import org.bukkit.event.player.PlayerInteractEvent
+
+interface IWeapon : Listener {
+
+    /**
+     *  Duplicate names are not allowed
+     */
+    val name: String
+
+    /**
+    * It can nullable but recommended
+     */
+    val description: String?
+
+    /**
+     * TODO bind to spacific item
+     */
+    val item: Material
+
+    @EventHandler
+    fun action(event: PlayerInteractEvent) {
+        if (event.item?.type != item) return
+
+        val player = event.player
+        val action = event.action
+
+        if (
+            player.isSneaking &&
+            (action == Action.LEFT_CLICK_AIR || action == Action.LEFT_CLICK_BLOCK)
+        ) return onShiftLeftClick(event)
+
+        if (
+            player.isSneaking &&
+            (action == Action.RIGHT_CLICK_AIR || action == Action.RIGHT_CLICK_BLOCK)
+        ) return onShiftRightClick(event)
+
+        when (action) {
+            Action.LEFT_CLICK_BLOCK, Action.LEFT_CLICK_AIR ->
+                onLeftClick(event)
+
+            Action.RIGHT_CLICK_BLOCK, Action.RIGHT_CLICK_AIR ->
+                onRightClick(event)
+
+            else -> return
+        }
+
+        when (action) {
+            Action.LEFT_CLICK_BLOCK -> return onLeftClickBlock(event)
+
+            Action.RIGHT_CLICK_BLOCK -> return onRightClickBlock(event)
+
+            Action.LEFT_CLICK_AIR -> return onLeftClickAir(event)
+
+            Action.RIGHT_CLICK_AIR -> return onRightClickAir(event)
+
+            else -> return
+        }
+    }
+
+    fun onLeftClick(event: PlayerInteractEvent) {}
+    fun onRightClick(event: PlayerInteractEvent) {}
+
+    fun onLeftClickBlock(event: PlayerInteractEvent) {}
+    fun onRightClickBlock(event: PlayerInteractEvent) {}
+
+    fun onLeftClickAir(event: PlayerInteractEvent) {}
+    fun onRightClickAir(event: PlayerInteractEvent) {}
+
+    fun onShiftLeftClick(event: PlayerInteractEvent) {}
+    fun onShiftRightClick(event: PlayerInteractEvent) {}
+
+}
---
+++ src/main/kotlin/skillx/utils/WeaponHandler.kt
@@ -0,0 +1,58 @@
+package skillx.utils
+
+import IWeapon
+import org.bukkit.plugin.java.JavaPlugin
+
+object WeaponHandler {
+
+    val weapons: HashMap<String, IWeapon> = HashMap()
+
+    /**
+     * @param mainClass JavaPlugin
+     * @sample code `WeaponHandler.initWeapons(this)` on extended JavaPlugin
+     */
+    fun initWeapons(mainClass: JavaPlugin) {
+        weapons.values.forEach { weapon ->
+            mainClass.server.pluginManager.registerEvents(weapon, mainClass)
+        }
+    }
+
+    /**
+     * @see Map is not allow duplicate name;
+     *      but checking to manually for handling
+     * @return if failed to register, return false
+     * @param T IWeapon | Array<IWeapon>
+     * else return true!
+     */
+    inline fun <reified T> register(t: T): Boolean {
+        // not an array
+        if (t is IWeapon) {
+            if (weapons[t.name] == null)
+                weapons[t.name] = t
+            return true
+        }
+
+        // array
+        if (t is Array<*> && t.isArrayOf<IWeapon>()) {
+            t.forEach { wea ->
+                if (wea !is IWeapon) return false
+                if (weapons[wea.name] == null)
+                    weapons[wea.name] = wea
+                return true
+            }
+        }
+
+        return false
+    }
+
+    /**
+     * @param weapon IWeapon | String (name)
+     */
+    inline fun <reified T> remove(weapon: T) {
+        if (weapon is String)
+            weapons.remove(weapon)
+        if (weapon is IWeapon && weapons[weapon.name] != null)
+            weapons.remove(weapon.name)
+    }
+
+}
