05fc0c7d6459cb0feb2df3dba88f4cd8a6677520
tmvkrpxl0 <ironman466@naver.com>
2022-10-31 23:33:29 +0900

    이거 지워 :stabolb:

--- src/test/kotlin/skillx/utils/MathTest.kt
+++ src/test/kotlin/skillx/utils/MathTest.kt
@@ -30,14 +30,4 @@ class MathTest {
         val angle = v1.angle(v2)
         assert(abs(angle.radian - (3.1415927F / 4)) < 0.01) { "벡터 각도 측정 알고리즘 실패! 목표값: π/4, 측정값: ${angle.radian}" }
     }
-
-    @Test
-    fun nanTest() {
-        val movement = Vector3f(-5.6064776E26F, 3.387764E26F, -6.25148E26F)
-        val axis = Vector3f(-3.291542E27F, -3.2154165E27F, 1.20945495E27F)
-
-        val angle = movement.angle(axis)
-
-        assert(!angle.radian.isNaN()) { "각도가 NaN 입니다!" }
-    }
 }
