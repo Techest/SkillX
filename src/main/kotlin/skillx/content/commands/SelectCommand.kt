680efc95a455010b5c6c61ea69f2e74ef1015f57
PleahMaCaka <pleahmacaka@gmail.com>
2022-11-01 22:53:14 +0900

    feat(debug/select): dynamic get all items

--- src/main/kotlin/skillx/content/commands/SelectCommand.kt
+++ src/main/kotlin/skillx/content/commands/SelectCommand.kt
@@ -1,33 +1,30 @@
 package skillx.content.commands
 
-import net.minecraft.Util
+import net.kyori.adventure.text.Component
 import org.bukkit.Bukkit
 import org.bukkit.Material
-import org.bukkit.Utility
 import org.bukkit.command.Command
 import org.bukkit.command.CommandExecutor
 import org.bukkit.command.CommandSender
 import org.bukkit.entity.Player
 import org.bukkit.inventory.Inventory
 import org.bukkit.inventory.ItemStack
-import javax.swing.text.Utilities
+import skillx.core.handlers.WeaponHandler
 
-val inv: Inventory = Bukkit.createInventory(null, 1 * 9)
-
-val item1 = createItem("Sword", Material.IRON_SWORD)
-val item2 = createItem("Bard", Material.GOLDEN_SWORD)
-val item3 = createItem("Rapier", Material.FLINT)
+val select_inv: Inventory = Bukkit.createInventory(null, 6 * 9)
 
 object SelectCommand : CommandExecutor {
     override fun onCommand(sender: CommandSender, command: Command, label: String, args: Array<out String>?): Boolean {
-        if(sender !is Player) return false
+        if (sender !is Player) return false
 
-        when(args?.get(0)) {
+        when (args?.get(0)) {
             "weapon" -> {
-                inv.setItem(1, item1)
-                inv.setItem(4, item2)
-                inv.setItem(7, item3)
-                sender.openInventory(inv)
+                var count = 0
+                WeaponHandler.weapons.values.forEach { wea ->
+                    select_inv.setItem(count, createItem(wea.name, wea.item))
+                    count = +1
+                }
+                sender.openInventory(select_inv)
             }
         }
 
@@ -38,7 +35,7 @@ object SelectCommand : CommandExecutor {
 private fun createItem(name: String, mat: Material): ItemStack {
     val item = ItemStack(mat, 1)
     val meta = item.itemMeta
-    meta.setDisplayName(name)
-    item.setItemMeta(meta)
+    meta.displayName(Component.text(name))
+    item.itemMeta = meta
     return item
 }
