994cd0b26aae4c7a40bb7e221815496b7e875448
DwarfAmil <hjp7126@naver.com>
2022-11-02 23:48:50 +0900

    feat(debug/select): showing more information

--- src/main/kotlin/skillx/core/commands/SelectCommand.kt
+++ src/main/kotlin/skillx/core/commands/SelectCommand.kt
@@ -1,4 +1,4 @@
-package skillx.content.commands
+package skillx.core.commands
 
 import net.kyori.adventure.text.Component
 import org.bukkit.Bukkit
@@ -6,12 +6,14 @@ import org.bukkit.Material
 import org.bukkit.command.Command
 import org.bukkit.command.CommandExecutor
 import org.bukkit.command.CommandSender
+import org.bukkit.command.TabCompleter
 import org.bukkit.entity.Player
 import org.bukkit.inventory.Inventory
 import org.bukkit.inventory.ItemStack
 import skillx.core.handlers.WeaponHandler
 
-val select_inv: Inventory = Bukkit.createInventory(null, 6 * 9)
+val text = Component.text("Weapon Select!")
+val select_inv: Inventory = Bukkit.createInventory(null, 6 * 9, text)
 
 object SelectCommand : CommandExecutor {
     override fun onCommand(sender: CommandSender, command: Command, label: String, args: Array<out String>?): Boolean {
@@ -21,7 +23,7 @@ object SelectCommand : CommandExecutor {
             "weapon" -> {
                 var count = 0
                 WeaponHandler.weapons.values.forEach { wea ->
-                    select_inv.setItem(count, createItem(wea.name, wea.item))
+                    select_inv.setItem(count, createItem(wea.name, wea.item, arrayOf("PVP Weapon", "Test!")))
                     count += 1
                 }
                 sender.openInventory(select_inv)
@@ -31,10 +33,34 @@ object SelectCommand : CommandExecutor {
     }
 }
 
-private fun createItem(name: String, mat: Material): ItemStack {
+object SelectTabAutoComplete : TabCompleter {
+
+    val arguments = ArrayList<String>()
+
+    override fun onTabComplete(
+        sender: CommandSender,
+        command: Command,
+        label: String,
+        args: Array<out String>?
+    ): MutableList<String>? {
+        if (arguments.isEmpty()) {
+            arguments.add("weapon")
+        }
+
+        return arguments
+    }
+
+}
+
+private fun createItem(name: String, mat: Material, lore: Array<String>): ItemStack {
     val item = ItemStack(mat, 1)
     val meta = item.itemMeta
+
     meta.displayName(Component.text(name))
+
     item.itemMeta = meta
+
+    item.lore(lore.map { i -> Component.text(i) })
+
     return item
 }
---
+++ src/main/kotlin/skillx/core/events/SelectionClickListener.kt
@@ -0,0 +1,25 @@
+package skillx.core.events
+
+import org.bukkit.Material
+import org.bukkit.event.EventHandler
+import org.bukkit.event.Listener
+import org.bukkit.event.inventory.InventoryClickEvent
+
+object SelectionClickListener: Listener {
+
+    @EventHandler
+    fun onClicked(event: InventoryClickEvent) {
+        if(event.inventory != select_inv) return
+
+        event.isCancelled = true
+
+        val player = event.whoClicked
+        val clickItem = event.currentItem
+
+        if(clickItem == null || clickItem.type == Material.AIR) return
+
+        player.inventory.addItem(clickItem)
+        player.closeInventory()
+
+    }
+}
