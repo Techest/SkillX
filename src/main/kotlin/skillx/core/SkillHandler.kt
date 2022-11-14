a2e95f04f441b82ce4b9b81455b5bce42aa0a9f9
pleahmacaka <pleahmacaka@gmail.com>
2022-11-15 02:22:53 +0900

    feat(SkillHandler): spawn particle on forward

--- src/main/kotlin/skillx/core/SkillHandler.kt
+++ src/main/kotlin/skillx/core/SkillHandler.kt
@@ -9,44 +9,6 @@ import org.bukkit.potion.PotionEffect
 import org.bukkit.potion.PotionEffectType
 
 object SkillHandler {
-    fun drawLineWithDamage(count: Int, particle: Particle, player: Player) {
-        val loc = player.location
-
-        for (i in 1..count) {
-            val skillLoc = Location(
-                player.world,
-                loc.x + (player.eyeLocation.direction.x * i),
-                loc.y + (player.eyeLocation.direction.y * i),
-                loc.z + (player.eyeLocation.direction.z * i)
-            )
-
-            player.spawnParticle(particle, skillLoc.x, skillLoc.y, skillLoc.z, 1)
-
-            player.world.getNearbyEntities(skillLoc, 1.0, 1.0, 1.0).forEach { target ->
-                if (target is LivingEntity) if (target != player) target.damage(5.0, player)
-            }
-        }
-    }
-
-    fun drawLineWithVelocity(count: Int, particle: Particle, player: Player, velocityInt: Int) {
-        val loc = player.location
-
-        for (i in 1..count) {
-            val skillLoc = Location(
-                player.world,
-                loc.x + (player.eyeLocation.direction.x * i),
-                loc.y + (player.eyeLocation.direction.y * i),
-                loc.z + (player.eyeLocation.direction.z * i)
-            )
-
-            player.spawnParticle(particle, skillLoc.x, skillLoc.y, skillLoc.z, 1)
-
-            player.world.getNearbyEntities(skillLoc, 1.0, 1.0, 1.0).forEach { target ->
-                if (target is LivingEntity) if (target != player) target.velocity =
-                    player.velocity.add(player.location.direction.multiply(velocityInt))
-            }
-        }
-    }
 
     fun drawLine(player: Player, reach: Int, particle: Particle): ArrayList<Location> {
         val loc = player.location
@@ -76,70 +38,29 @@ object SkillHandler {
         }
     }
 
-    fun particleWithDamage(distance: Int, particle: Particle, player: Player, particleInt: Int, damage: Double) {
+    fun forwardParticle(player: Player, particle: Particle, range: Int, distance: Double): Location {
         val loc = player.location
-
-        val skillLoc = Location(
+        val particleLoc = Location(
             player.world,
             loc.x + (player.eyeLocation.direction.x * distance),
-            loc.y + (player.eyeLocation.direction.y * distance),
+            loc.y + (player.eyeLocation.direction.y),
             loc.z + (player.eyeLocation.direction.z * distance)
         )
-
-        player.spawnParticle(particle, skillLoc.x, skillLoc.y, skillLoc.z, particleInt)
-
-        player.world.getNearbyEntities(skillLoc, 3.0, 3.0, 3.0).forEach { target ->
-            if (target is LivingEntity) if (target != player) target.damage(damage, player)
-        }
+        player.world.spawnParticle(particle, particleLoc, range)
+        return particleLoc
     }
 
-    fun particleWithVelocity(distance: Int, particle: Particle, player: Player, particleInt: Int, velocityInt: Int) {
-        val loc = player.location
-
-        val skillLoc = Location(
-            player.world,
-            loc.x + (player.eyeLocation.direction.x * distance),
-            loc.y + (player.eyeLocation.direction.y * distance),
-            loc.z + (player.eyeLocation.direction.z * distance)
-        )
-
-        player.spawnParticle(particle, skillLoc.x, skillLoc.y, skillLoc.z, particleInt)
-
-        player.world.getNearbyEntities(skillLoc, 3.0, 3.0, 3.0).forEach { target ->
-            if (target is LivingEntity) if (target != player) target.velocity =
-                player.velocity.add(player.location.direction.multiply(velocityInt))
-        }
-    }
-
-    fun particleWithEffect(
-        distance: Int,
-        particle: Particle,
+    fun forwardParticleExcuteFunc(
         player: Player,
-        particleInt: Int,
-        effect: PotionEffectType,
-        effectTime: Int,
-        effectStrong: Int,
+        particle: Particle,
+        range: Int,
+        distance: Double,
+        func: (entity: Entity?) -> Unit,
     ) {
-        val loc = player.location
-
-        val skillLoc = Location(
-            player.world,
-            loc.x + (player.eyeLocation.direction.x * distance),
-            loc.y + (player.eyeLocation.direction.y * distance),
-            loc.z + (player.eyeLocation.direction.z * distance)
-        )
-
-        player.spawnParticle(particle, skillLoc.x, skillLoc.y, skillLoc.z, particleInt)
-
-        player.world.getNearbyEntities(skillLoc, 3.0, 3.0, 3.0).forEach { target ->
-            if (target is LivingEntity) if (target != player) target.addPotionEffect(
-                PotionEffect(
-                    effect,
-                    effectTime,
-                    effectStrong
-                )
-            )
-        }
+        player.world.getNearbyEntities(forwardParticle(player, particle, range, distance), distance, 1.0, distance)
+            .forEach { entity: Entity? ->
+                func(entity)
+            }
     }
 
 }
