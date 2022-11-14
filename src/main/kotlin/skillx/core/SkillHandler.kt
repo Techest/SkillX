1a856daacaef0429ff5aafad8a94a6504ad373b6
pleahmacaka <pleahmacaka@gmail.com>
2022-11-15 01:21:34 +0900

    feat(SkillHandler): lamda, clearly func

--- src/main/kotlin/skillx/core/SkillHandler.kt
+++ src/main/kotlin/skillx/core/SkillHandler.kt
@@ -2,6 +2,7 @@ package skillx.core
 
 import org.bukkit.Location
 import org.bukkit.Particle
+import org.bukkit.entity.Entity
 import org.bukkit.entity.LivingEntity
 import org.bukkit.entity.Player
 import org.bukkit.potion.PotionEffect
@@ -43,6 +44,34 @@ object SkillHandler {
             player.world.getNearbyEntities(skillLoc, 1.0, 1.0, 1.0).forEach { target ->
                 if (target is LivingEntity) if (target != player) target.velocity =
                     player.velocity.add(player.location.direction.multiply(velocityInt))
+            }
+        }
+    }
+
+    fun drawLine(player: Player, reach: Int, particle: Particle): ArrayList<Location> {
+        val loc = player.location
+        val usedLoc: ArrayList<Location> = ArrayList()
+
+        for (i in 1..reach) {
+            val particleLoc = Location(
+                player.world,
+                loc.x + (player.eyeLocation.direction.x * i),
+                loc.y + (player.eyeLocation.direction.y * i),
+                loc.z + (player.eyeLocation.direction.z * i)
+            )
+            usedLoc.add(particleLoc)
+            player.world.spawnParticle(particle, particleLoc, reach)
+        }
+        return usedLoc
+    }
+
+    /**
+     * @param reach 1 = one block
+     */
+    fun drawLineWithExecuteFunc(player: Player, reach: Int, particle: Particle, func: (entity: Entity?) -> Unit) {
+        drawLine(player, reach, particle).forEach { loc ->
+            player.world.getNearbyEntities(loc, 1.0, 1.0, 1.0).forEach { entity: Entity? ->
+                func(entity)
             }
         }
     }
@@ -89,7 +118,7 @@ object SkillHandler {
         particleInt: Int,
         effect: PotionEffectType,
         effectTime: Int,
-        effectStrong: Int
+        effectStrong: Int,
     ) {
         val loc = player.location
 
